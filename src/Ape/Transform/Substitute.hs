c3fa60e8f7bcc67851425a4d3ec4817511834513
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2016-02-25 20:24:59 +0100

    More generic substitution

--- src/Ape/Transform/Substitute.hs
+++ src/Ape/Transform/Substitute.hs
@@ -1,28 +1,30 @@
-module Ape.Transform.Substitute (substitute) where
+module Ape.Transform.Substitute (substituteWith, substitute) where
 
 import Ape.Expr
 import Ape.Env
 
 class Substitute a where
+    substituteWith :: (b -> Value) -> Env b -> a -> a
     substitute :: Env Value -> a -> a
+    substitute = substituteWith id
 
 instance Substitute Value where
-    substitute e x@(Var v) = if isInEnv e v
-                             then lookupEnv e v
+    substituteWith f e x@(Var v) = if isInEnv e v
+                             then f $ lookupEnv e v
                              else x
-    substitute e (Tuple v) = Tuple $ map (substitute e) v
-    substitute e (Lambda v t b) = Lambda v t (substitute e b)
-    substitute _ x = x
+    substituteWith f e (Tuple v) = Tuple $ map (substituteWith f e) v
+    substituteWith f e (Lambda v t b) = Lambda v t (substituteWith f e b)
+    substituteWith _ _ x = x
 
 instance Substitute CExpr where
-    substitute e (If c t f) = If (substitute e c) (substitute e t) (substitute e f)
-    substitute e (App xs) = App $ map (substitute e) xs
-    substitute e (Atomic a) = Atomic (substitute e a)
+    substituteWith f e (If c a b) = If (substituteWith f e c) (substituteWith f e a) (substituteWith f e b)
+    substituteWith f e (App xs) = App $ map (substituteWith f e) xs
+    substituteWith f e (Atomic a) = Atomic (substituteWith f e a)
 
 instance Substitute AExpr where
-    substitute e (Val v) = Val $ substitute e v
-    substitute e (PrimOp op xs) = PrimOp op $ map (substitute e) xs
+    substituteWith f e (Val v) = Val $ substituteWith f e v
+    substituteWith f e (PrimOp op xs) = PrimOp op $ map (substituteWith f e) xs
 
 instance Substitute Expr where
-    substitute e (Let v b) = Let (map (\(i, t, b') -> (i, t, substitute e b')) v) (substitute e b)
-    substitute e (Complex c) = Complex (substitute e c)
+    substituteWith f e (Let v b) = Let (map (\(i, t, b') -> (i, t, substituteWith f e b')) v) (substituteWith f e b)
+    substituteWith f e (Complex c) = Complex (substituteWith f e c)
