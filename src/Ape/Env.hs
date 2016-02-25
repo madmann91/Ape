c7f9ac3116c89c8d1697b5cf95220fe76ab05267
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2016-02-25 20:26:32 +0100

    Removed dependency on data-stringmap

--- src/Ape/Env.hs
+++ src/Ape/Env.hs
@@ -3,48 +3,51 @@ module Ape.Env where
 import Ape.Print
 
 import Data.Char
-import qualified Data.StringMap as SM
+import qualified Data.Map.Strict as M
 
-type Env a = SM.StringMap a
+type Env a = M.Map String a
 
 emptyEnv :: Env a
-emptyEnv = SM.empty
+emptyEnv = M.empty
 
 lookupEnv :: Env a -> String -> a
-lookupEnv e i = case SM.lookup i e of
+lookupEnv e i = case M.lookup i e of
     Just v -> v
     Nothing -> error $ "Undeclared identifier " ++ i
 
 insertEnv :: Env a -> String -> a -> Env a
 insertEnv e "_" _ = e
-insertEnv e i x = SM.insertWithKey (\k _ _ -> error $ "Identifier " ++ k ++ " already used") i x e
+insertEnv e i x = M.insertWithKey (\k _ _ -> error $ "Identifier " ++ k ++ " already used") i x e
 
 concatEnv :: Env a -> Env a -> Env a
-concatEnv = SM.unionWithKey (\k _ _ -> error $ "Duplicate values for " ++ k)
+concatEnv = M.unionWithKey (\k _ _ -> error $ "Duplicate values for " ++ k)
 
 isInEnv :: Env a -> String -> Bool
-isInEnv e i = SM.member i e
+isInEnv e i = M.member i e
 
 enumEnv :: Env a -> [(String, a)]
-enumEnv e = SM.toList e
+enumEnv e = M.assocs e
 
 removeFromEnv :: Env a -> String -> Env a
-removeFromEnv e i = SM.delete i e
+removeFromEnv e i = M.delete i e
 
 mapEnv :: (a-> b) -> Env a -> Env b
-mapEnv = SM.map
+mapEnv = M.map
 
 mapEnvWithKey :: (String -> a -> b) -> Env a -> Env b
-mapEnvWithKey = SM.mapWithKey
+mapEnvWithKey = M.mapWithKey
 
 printEnv :: PrettyPrint a => Env a -> IO ()
 printEnv e = print $ map (\(k, v) -> (k, prettyPrint0 v)) (enumEnv e)
 
 genVariable :: Env a -> String -> String
-genVariable e s = if SM.member s e
-    then next
+genVariable e = genVariableWith (isInEnv e)
+
+genVariableWith :: (String -> Bool) -> String -> String
+genVariableWith f s = if f s
+    then genVariableWith f s'
     else s
     where
-        next = if isDigit c && c /= '9' then reverse $ chr (ord c + 1) : (tail $ reverse m) else m ++ "0"
-        c = last m
-        m = fst $ last $ SM.toListShortestFirst $ SM.prefixFilter s e
+        s' = if isDigit c && c /= '9' then reverse $ c' : (tail $ reverse s) else s ++ "0"
+        c = last s
+        c' = chr (ord c + 1)
