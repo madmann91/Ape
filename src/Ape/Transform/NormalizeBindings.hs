ee438eb9357a1d7b0abf4408930330411d97984e
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2016-02-25 20:26:49 +0100

    More partial eval

--- src/Ape/Transform/NormalizeBindings.hs
+++ src/Ape/Transform/NormalizeBindings.hs
@@ -2,17 +2,34 @@ module Ape.Transform.NormalizeBindings where
 
 import qualified Ape.Expr as E
 import qualified Ape.Type as T
+import Ape.Env
+
+import qualified Data.Set as S
+import Data.List
+
+-- Renaming of the bindings is done in order to avoid name clashes
+-- when pulling up a binding from a lower level
+renameBindings :: S.Set String -> [E.LetBinding] -> (S.Set String, [E.LetBinding])
+renameBindings variables = foldl' rename (variables, [])
+    where
+        isUsed s v = S.member v s
+        rename (usedVars, bindings) (v, t, b) = (S.insert v' usedVars, (v', t, b) : bindings)
+            where
+                v' = genVariableWith (isUsed usedVars) v
 
 -- Beta-expansion creates constructs such as let a = (let b = c in d) in e.
 -- This is not allowed in ANF. Hence, we transform this expression into:
 -- let a = d, b = c in e.
-normalizeBindings :: [(E.Variable, T.Type, E.Expr)] -> [E.LetBinding]
-normalizeBindings bindings = normalize bindings []
+normalizeBindings :: Env a -> [(E.Variable, T.Type, E.Expr)] -> [E.LetBinding]
+normalizeBindings e bindings = normalize names bindings []
     where
-        normalize (b:bs) ns = case b of
+        -- Insert all the names in a set
+        names = foldl' (\n (v, _) -> S.insert v n) S.empty (enumEnv e)
+        normalize s (b:bs) ns = case b of
             -- Already normalized case
-            (var, t, E.Complex c) -> normalize bs ((var, t, c):ns)
+            (var, t, E.Complex c) -> normalize s bs ((var, t, c):ns)
             -- Normalization occurs here
-            (var, t, E.Let vars body) -> normalize ((var, t, body):bs) (vars ++ ns)
-        normalize [] ns = reverse $ ns
-
+            (var, t, E.Let vars body) -> normalize s' ((var, t, body):bs) (renamedVars ++ ns)
+                where
+                    (s', renamedVars) = renameBindings s vars
+        normalize _ [] ns = reverse $ ns
--- src/Ape/Transform/PartialEval.hs
+++ src/Ape/Transform/PartialEval.hs
@@ -1,6 +1,7 @@
 module Ape.Transform.PartialEval(partialEval) where
 
 import qualified Ape.Expr as E
+import qualified Ape.Type as T
 import Ape.Eval
 import Ape.Env
 import Ape.Transform.Substitute
@@ -8,9 +9,12 @@ import Ape.Transform.NormalizeBindings
 
 import Data.List
 
+type TypedValue = (T.Type, E.Value)
+type TypedCExpr = (T.Type, E.CExpr)
+
 class PartialEval a where
     -- Partially evaluates an expression with an environment which contains known values
-    partialEval :: Env E.Value -> a -> a
+    partialEval :: Env TypedValue -> a -> a
 
 instance PartialEval E.Expr where
     partialEval e (E.Let v b) = case v' of
@@ -19,15 +23,15 @@ instance PartialEval E.Expr where
         where
             b' = partialEval e' b
 
-            handleBindings :: (Env E.Value, [E.LetBinding]) -> (Env E.Value, [E.LetBinding])
+            handleBindings :: (Env TypedValue, [E.LetBinding]) -> (Env TypedValue, [E.LetBinding])
             handleBindings prev@(var2val, bindings) = if result == prev
                 then result
                 else handleBindings result
                 where
                     -- Partially evaluate bindings
-                    evalBindings = map (\(w,t,c) -> (w, t, partialEvalComplex var2val c)) bindings
+                    evalBindings = map (\(w, t, c) -> (w, t, partialEvalComplex var2val c)) bindings
                     -- Monadic transformation on bindings to get CExprs instead of Exprs
-                    normBindings = normalizeBindings evalBindings
+                    normBindings = normalizeBindings var2val evalBindings
                     -- Partition the evaluated bindings into values and expressions
                     isValue (_, _, E.Atomic (E.Val _)) = True
                     isValue _ = False
@@ -35,10 +39,11 @@ instance PartialEval E.Expr where
                     -- Store the values in the new environment
                     extractValue (E.Atomic (E.Val val)) = val
                     extractValue _ = error "Non-value given to extractValue"
-                    var2val' = foldl' (\f (w, _, val) -> insertEnv f w $ extractValue val) var2val values
+                    var2val' = foldl' (\f (w, t, val) -> insertEnv f w (t, extractValue val)) var2val values
                     result = (var2val', bindings')
 
             (e', v') = handleBindings (e, v)
+            --ce' = foldl' (\f (w, t, c) -> insertEnv f w (t, c)) ce v
     partialEval e (E.Complex c) = partialEvalComplex e c
 
 instance PartialEval E.AExpr where
@@ -55,17 +60,15 @@ instance PartialEval E.Value where
     partialEval e (E.Lambda v t b) = E.Lambda v t $ partialEval e b
     partialEval _ val = val
 
-partialEvalComplex :: Env E.Value -> E.CExpr -> E.Expr
-partialEvalComplex e (E.If c t f) = if known
+partialEvalComplex :: Env TypedValue -> E.CExpr -> E.Expr
+partialEvalComplex e branch@(E.If c t f) = if known
     -- Evaluate if when the condition is known
     then if cond then partialEval e t else partialEval e f
-    -- Do not evaluate if, but evaluate branches
-    else E.Complex $ E.If c (partialEval e t) (partialEval e f)
+    else E.Complex $ branch
     where
         (known, cond) = case c of
             E.I1 [cond'] -> (True, cond')
-            E.Var var  -> (True, isInEnv e var && lookupEnv e var == E.I1 [True])
+            E.Var var  -> (True, isInEnv e var && snd (lookupEnv e var) == E.I1 [True])
             _ -> (False, undefined)
 partialEvalComplex e app@(E.App vals) = E.Complex app
-partialEvalComplex e (E.Atomic a) = E.Complex $ E.Atomic $ partialEval e (substitute e a)
-
+partialEvalComplex e (E.Atomic a) = E.Complex $ E.Atomic $ partialEval e (substituteWith snd e a)
